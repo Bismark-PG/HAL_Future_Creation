4d0293c51e38a2f5db43bd2bb899801c9179048e
shiolemonC <haganechen@gmail.com>
2026-09-11 10:50:53 +0900

    [mainDev] add base Vehicle Control(still testing)

--- Source/HAL_Future_Creation/TestVehiclePawn.cpp
+++ Source/HAL_Future_Creation/TestVehiclePawn.cpp
@@ -1,34 +1,256 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "TestVehiclePawn.h"
 
-// Sets default values
+#include "ArcadeVehicleMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
+
 ATestVehiclePawn::ATestVehiclePawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 
+	CollisionRoot = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionRoot"));
+	SetRootComponent(CollisionRoot);
+	CollisionRoot->InitBoxExtent(FVector(100.0f, 70.0f, 35.0f));
+	CollisionRoot->SetCollisionProfileName(TEXT("PhysicsActor"));
+	CollisionRoot->SetSimulatePhysics(true);
+	CollisionRoot->SetEnableGravity(true);
+	CollisionRoot->SetLinearDamping(0.15f);
+	CollisionRoot->SetAngularDamping(0.8f);
+	CollisionRoot->SetMassOverrideInKg(NAME_None, 800.0f, true);
+	CollisionRoot->SetNotifyRigidBodyCollision(true);
+	CollisionRoot->BodyInstance.bUseCCD = true;
+
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualMesh"));
+	VisualMesh->SetupAttachment(CollisionRoot);
+	VisualMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	VisualMesh->SetSimulatePhysics(false);
+
+	ForwardArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ForwardArrow"));
+	ForwardArrow->SetupAttachment(CollisionRoot);
+	ForwardArrow->SetRelativeLocation(FVector(110.0f, 0.0f, 0.0f));
+	ForwardArrow->ArrowColor = FColor::Cyan;
+	ForwardArrow->ArrowSize = 1.5f;
+	ForwardArrow->SetHiddenInGame(false);
+
+	ArcadeMovement = CreateDefaultSubobject<UArcadeVehicleMovementComponent>(TEXT("ArcadeMovement"));
+	ArcadeMovement->SetUpdatedPrimitive(CollisionRoot);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(CollisionRoot);
+	CameraBoom->SetUsingAbsoluteRotation(true);
+	CameraBoom->SetRelativeRotation(FRotator(-60.0f, 0.0f, 0.0f));
+	CameraBoom->TargetArmLength = 1200.0f;
+	CameraBoom->bDoCollisionTest = false;
+	CameraBoom->bEnableCameraLag = true;
+	CameraBoom->CameraLagSpeed = 8.0f;
+
+	TopDownCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	TopDownCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	TopDownCamera->bUsePawnControlRotation = false;
 }
 
-// Called when the game starts or when spawned
-void ATestVehiclePawn::BeginPlay()
+void ATestVehiclePawn::PawnClientRestart()
 {
-	Super::BeginPlay();
-	
+	Super::PawnClientRestart();
+
+	ResetInputCommand();
+	AddDefaultInputContext();
 }
 
-// Called every frame
-void ATestVehiclePawn::Tick(float DeltaTime)
+void ATestVehiclePawn::UnPossessed()
 {
-	Super::Tick(DeltaTime);
+	RemoveDefaultInputContext();
+	ResetInputCommand();
 
+	Super::UnPossessed();
 }
 
-// Called to bind functionality to input
+void ATestVehiclePawn::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	RemoveDefaultInputContext();
+	ResetInputCommand();
+
+	Super::EndPlay(EndPlayReason);
+}
+
 void ATestVehiclePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (!EnhancedInput)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s requires an Enhanced Input component."), *GetNameSafe(this));
+		return;
+	}
+
+	if (SteeringAction)
+	{
+		EnhancedInput->BindAction(SteeringAction, ETriggerEvent::Triggered, this, &ThisClass::OnSteeringInput);
+		EnhancedInput->BindAction(SteeringAction, ETriggerEvent::Completed, this, &ThisClass::OnSteeringCompleted);
+	}
+	if (ThrottleAction)
+	{
+		EnhancedInput->BindAction(ThrottleAction, ETriggerEvent::Started, this, &ThisClass::OnThrottleStarted);
+		EnhancedInput->BindAction(ThrottleAction, ETriggerEvent::Completed, this, &ThisClass::OnThrottleCompleted);
+	}
+	if (BrakeAction)
+	{
+		EnhancedInput->BindAction(BrakeAction, ETriggerEvent::Started, this, &ThisClass::OnBrakeStarted);
+		EnhancedInput->BindAction(BrakeAction, ETriggerEvent::Completed, this, &ThisClass::OnBrakeCompleted);
+	}
+	if (HandbrakeAction)
+	{
+		EnhancedInput->BindAction(HandbrakeAction, ETriggerEvent::Started, this, &ThisClass::OnHandbrakeStarted);
+		EnhancedInput->BindAction(HandbrakeAction, ETriggerEvent::Completed, this, &ThisClass::OnHandbrakeCompleted);
+	}
+	if (LaunchAction)
+	{
+		EnhancedInput->BindAction(LaunchAction, ETriggerEvent::Started, this, &ThisClass::OnLaunchStarted);
+		EnhancedInput->BindAction(LaunchAction, ETriggerEvent::Completed, this, &ThisClass::OnLaunchCompleted);
+	}
+
+	if (!DefaultMappingContext || !SteeringAction || !ThrottleAction || !BrakeAction || !HandbrakeAction || !LaunchAction)
+	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("%s has unassigned Enhanced Input assets. Configure its Vehicle|Input defaults."),
+			*GetNameSafe(this));
+	}
+}
+
+void ATestVehiclePawn::AddDefaultInputContext()
+{
+	if (!IsLocallyControlled() || !DefaultMappingContext)
+	{
+		return;
+	}
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	ULocalPlayer* LocalPlayer = PlayerController ? PlayerController->GetLocalPlayer() : nullptr;
+	if (!LocalPlayer)
+	{
+		return;
+	}
+
+	if (UEnhancedInputLocalPlayerSubsystem* InputSubsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
+	{
+		InputSubsystem->RemoveMappingContext(DefaultMappingContext);
+		InputSubsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void ATestVehiclePawn::RemoveDefaultInputContext()
+{
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	ULocalPlayer* LocalPlayer = PlayerController ? PlayerController->GetLocalPlayer() : nullptr;
+	if (!LocalPlayer)
+	{
+		return;
+	}
+
+	if (UEnhancedInputLocalPlayerSubsystem* InputSubsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
+	{
+		if (DefaultMappingContext)
+		{
+			InputSubsystem->RemoveMappingContext(DefaultMappingContext);
+		}
+	}
+}
+
+void ATestVehiclePawn::PushInputCommand()
+{
+	CurrentInputCommand.Sanitize();
+	if (ArcadeMovement)
+	{
+		ArcadeMovement->SetInputCommand(CurrentInputCommand);
+	}
+}
+
+void ATestVehiclePawn::ResetInputCommand()
+{
+	CurrentInputCommand.Reset();
+	if (ArcadeMovement)
+	{
+		ArcadeMovement->ResetInputCommand();
+	}
+}
+
+void ATestVehiclePawn::OnSteeringInput(const FInputActionValue& Value)
+{
+	CurrentInputCommand.Steering = Value.Get<float>();
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnSteeringCompleted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.Steering = 0.0f;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnThrottleStarted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.Throttle = 1.0f;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnThrottleCompleted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.Throttle = 0.0f;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnBrakeStarted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.Brake = 1.0f;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnBrakeCompleted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.Brake = 0.0f;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnHandbrakeStarted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.bHandbrake = true;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnHandbrakeCompleted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.bHandbrake = false;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnLaunchStarted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.bLaunch = true;
+	PushInputCommand();
+}
+
+void ATestVehiclePawn::OnLaunchCompleted(const FInputActionValue& Value)
+{
+	CurrentInputCommand.bLaunch = false;
+	PushInputCommand();
 }
 
--- Source/HAL_Future_Creation/TestVehiclePawn.h
+++ Source/HAL_Future_Creation/TestVehiclePawn.h
@@ -1,29 +1,94 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "VehicleInputCmd.h"
 #include "TestVehiclePawn.generated.h"
 
+class UArcadeVehicleMovementComponent;
+class UArrowComponent;
+class UBoxComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+class USpringArmComponent;
+class UStaticMeshComponent;
+struct FInputActionValue;
+
+/** Minimal player vehicle used to validate MVP-A ground handling. */
 UCLASS()
 class HAL_FUTURE_CREATION_API ATestVehiclePawn : public APawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ATestVehiclePawn();
 
+	virtual void PawnClientRestart() override;
+	virtual void UnPossessed() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintPure, Category = "Vehicle|Input")
+	FVehicleInputCmd GetCurrentInputCommand() const { return CurrentInputCommand; }
+
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle|Components")
+	TObjectPtr<UBoxComponent> CollisionRoot;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle|Components")
+	TObjectPtr<UStaticMeshComponent> VisualMesh;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	/** Visible MVP marker for the physical forward / future launch direction. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle|Components")
+	TObjectPtr<UArrowComponent> ForwardArrow;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle|Components")
+	TObjectPtr<UArcadeVehicleMovementComponent> ArcadeMovement;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle|Components")
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle|Components")
+	TObjectPtr<UCameraComponent> TopDownCamera;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Vehicle|Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Vehicle|Input")
+	TObjectPtr<UInputAction> SteeringAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Vehicle|Input")
+	TObjectPtr<UInputAction> ThrottleAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Vehicle|Input")
+	TObjectPtr<UInputAction> BrakeAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Vehicle|Input")
+	TObjectPtr<UInputAction> HandbrakeAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Vehicle|Input")
+	TObjectPtr<UInputAction> LaunchAction;
+
+private:
+	void AddDefaultInputContext();
+	void RemoveDefaultInputContext();
+	void PushInputCommand();
+	void ResetInputCommand();
+
+	void OnSteeringInput(const FInputActionValue& Value);
+	void OnSteeringCompleted(const FInputActionValue& Value);
+	void OnThrottleStarted(const FInputActionValue& Value);
+	void OnThrottleCompleted(const FInputActionValue& Value);
+	void OnBrakeStarted(const FInputActionValue& Value);
+	void OnBrakeCompleted(const FInputActionValue& Value);
+	void OnHandbrakeStarted(const FInputActionValue& Value);
+	void OnHandbrakeCompleted(const FInputActionValue& Value);
+	void OnLaunchStarted(const FInputActionValue& Value);
+	void OnLaunchCompleted(const FInputActionValue& Value);
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Vehicle|Input")
+	FVehicleInputCmd CurrentInputCommand;
 };
